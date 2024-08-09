44a88fe19d77209a88a3f6429ddc1e80094cc67e
Jeronimo SR <jsr2609@gmail.com>
2024-08-09 10:30:14 -0600

    refresh table

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -8,7 +8,7 @@ export function CardSkeleton() {
       className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
     >
       <div className="flex p-4">
-        <div className="h-5 w-5 rounded-md bg-gray-200" />
+        <div className="h-5 w-5 rounded-md bg-gray-300" />
         <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
       </div>
       <div className="flex items-center justify-center truncate rounded-xl bg-white px-4 py-8">
@@ -32,11 +32,11 @@ export function CardsSkeleton() {
 export function RevenueChartSkeleton() {
   return (
     <div className={`${shimmer} relative w-full overflow-hidden md:col-span-4`}>
-      <div className="mb-4 h-8 w-36 rounded-md bg-gray-100" />
-      <div className="rounded-xl bg-gray-100 p-4">
+      <div className="mb-4 h-8 w-36 rounded-md bg-gray-300" />
+      <div className="rounded-xl bg-gray-300 p-4">
         <div className="mt-0 grid h-[410px] grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4" />
         <div className="flex items-center pb-2 pt-6">
-          <div className="h-5 w-5 rounded-full bg-gray-200" />
+          <div className="h-5 w-5 rounded-full bg-gray-300" />
           <div className="ml-2 h-4 w-20 rounded-md bg-gray-200" />
         </div>
       </div>
